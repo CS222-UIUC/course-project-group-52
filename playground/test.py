a27956416f9b18de4c710d7f2861c9dc1af71bdd
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-10-15 22:52:29 -0500

    Fixed test.py

--- playground/test.py
+++ playground/test.py
@@ -7,27 +7,30 @@ import unittest
 bias = 1
 weights = [[random.random(), random.random(), random.random(), random.random(), random.random(), random.random(), random.random()]]
 
+
 def train(training_size, dynamic):
+    # NEW
+    global p
+    global output
+    p = pc.Perceptron()
     lr = 1
-    print(weights)
-    for _ in range(training_size): 
+    trials = 25
+    print('Initial Weights: ', p.weights)
+    for _ in range(training_size):
         inputs = numpy.random.randint(2, size=6)
-        numpy.append(inputs, bias)
         output = ((inputs[0] or inputs[1]) and (inputs[2] and inputs[3])) or (inputs[4] or inputs[5])
-        pc.Perceptron(weights, inputs, output, lr)
-        if dynamic: lr -= (1/training_size)
-    print(weights)
+        p.activate(inputs, output, lr)
+        if dynamic: lr -= (1/trials)
 
 def run():
-    errors = 0
     lr = 0
+    errors = 0
     for i in range(64):
         array = numpy.array([int(b) for b in format(i, '#08b')[2:]])
         out = ((array[0] or array[1]) and (array[2] and array[3])) or (array[4] or array[5])
-        outP = pc.Perceptron(weights, array, out, lr)
-        if (outP != out):
-            errors += 1
-        print(outP, out)
+        out = p.activate(array, out, lr)
+        if (out != output):
+                errors += 1
     return errors
 
 def simulate(rounds, training_size, dynamic):
