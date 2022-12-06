36a432eebcdf2a2ad63db30289c8b58afddb5953
Gautam Dayal <gautam.dayal@yahoo.com>
2022-12-06 12:51:53 -0600

    Mapping value function

--- mcu/src/Layer.cpp
+++ mcu/src/Layer.cpp
@@ -18,3 +18,9 @@ void Layer::WriteLayerDigital(int brightness_vec[]) {
         node_vec[i].WriteBrightness(brightness_vec[i]);
     }
 }
+
+int MapFromRange(float value, int old_min, int old_max, int new_min, int new_max) {
+    float mapped = (value - (float)old_min) * (float)((new_max-new_min)/(old_max-old_min)) + (float)new_min;
+    int cast = (int)mapped;
+    return cast;
+}
--- mcu/src/Layer.h
+++ mcu/src/Layer.h
@@ -7,6 +7,7 @@ class Layer {
         Layer(int l_size, Light nodes[]);
         void WriteLayer(int brightness_vec[]);
         void WriteLayerDigital(int brightness_vec[]);
+        int MapFromRange(float value, int old_min, int old_max, int new_min, int new_max);
     private:
         Light node_vec[15];
         int layer_size;
